51e6b06d5876df47da21e2841bf659bc2f00e20e
RaresClinci <beevee@DESKTOP-G1B9JFE.localdomain>
2023-05-03 22:14:49 +0300

    Add trig functions and TOFIX file

--- trig.c
+++ trig.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#define TOL 0.0000001
+#define MAXITER 100
 
 double sinus(double x) {
-    // TODO
+    double px = x, S = x;
+    int i;
+    for(i = 1; i < MAXITER; i++) {
+        px *= -x*x/(2 * i * (2 * i + 1));
+        S += px;
+        if(px < TOL && px > -TOL) 
+            return S;
+    }
+    return S;
 }
 
 double cosinus(double x) {
-    // TODO
+    double px = 1, S = 1;
+    int i;
+    for(i = 1; i < MAXITER; i++) {
+        px *= -x*x/((2 * i - 1) * 2 * i);
+        S += px;
+        if(px < TOL && px > -TOL)
+            return S;
+    }
+    return S;
 }
 
 double tangent(double x) {
-    // TODO
+    return sinus(x) / cosinus(x);
 }
 
 double cotangent(double x) {
-    // TODO
+    return cosinus(x) / sinus(x);
 }
