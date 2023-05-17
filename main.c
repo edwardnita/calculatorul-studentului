2e3f52147f782c94b09f75fd897ca8e7aacdf6ba
Eduard <eduardeugennita@yahoo.com>
2023-05-17 10:04:02 +0300

    Initial working program

--- main.c
+++ main.c
@@ -3,4 +3,20 @@
 
 #include "functions.h"
 
-int main() { return 0; }
+int main(int argc, char **argv) {
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Calculatorul Studentului");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 600);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
+}
